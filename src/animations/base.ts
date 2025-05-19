8ad8bf8ec147e105519cdb9b57c4684efbe236c1
Tae.Garam (MacBook) <drasla217@gmail.com>
2025-05-19 21:59:17 +0900

    ErrorAnimation 버그 수정

--- src/animations/base.ts
+++ src/animations/base.ts
@@ -3,11 +3,11 @@ import { THEME_COLOR } from "../index";
 const validColorStyle: THEME_COLOR[] = [
     "primary",
     "secondary",
-    "primary",
-    "secondary",
     "success",
     "info",
     "warning",
+    "error",
+    "disabled",
 ];
 
 export const getColor = (color: THEME_COLOR | string): string => {
