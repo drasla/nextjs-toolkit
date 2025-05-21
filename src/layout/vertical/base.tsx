de0f797c6b115bcb8563ad64ef55f31eba74666b
Tae.Garam (MacBook) <drasla217@gmail.com>
2025-05-21 16:18:15 +0900

    Button variant=text 추가

--- src/layout/vertical/base.tsx
+++ src/layout/vertical/base.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 import { ThemeUIProvider } from "../../providers/theme";
 import { AsideUIProvider } from "../../providers/aside";
-import { TbMenu2 } from "react-icons/tb";
 
 interface Props extends PropsWithChildren {
     aside?: ReactNode;
