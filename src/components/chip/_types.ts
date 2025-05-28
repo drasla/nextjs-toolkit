791f11a69d31dfb177a38e2358519f48dcc4411d
Tae.Garam (MacBook) <drasla217@gmail.com>
2025-05-28 21:32:17 +0900

    Chip 버그 수정

--- src/components/chip/_types.ts
+++ src/components/chip/_types.ts
@@ -3,6 +3,5 @@ import { THEME_COLOR } from "../../types";
 export type ChipProps<T extends string> = {
     meta: T;
     getMeta: (meta: T) => { label: string; color: THEME_COLOR };
-    colors: THEME_COLOR;
     className?: string;
 };
